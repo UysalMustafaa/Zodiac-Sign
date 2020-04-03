257e1a446b88f4b44c48303268d2a23877a480aa
Mustafa <18701031@mersin.edu.tr>
2020-04-03 23:28:30 +0300

    Preparation of the Data Source

--- lib/burc_liste.dart
+++ lib/burc_liste.dart
@@ -1,10 +1,40 @@
+import 'package:com/models/burc.dart';
+import 'package:com/utils/strings.dart';
 import 'package:flutter/material.dart';
 
+
 class BurcListesi extends StatelessWidget {
+
+
+
   @override
   Widget build(BuildContext context) {
 
-    return Container();
+    List<Burc> tumBurclar = veriKaynaginiHazirla();
+
+    return Scaffold (
+      appBar: AppBar (title: Text("Burç Rehberi"),),
+      body: Container(),
+    );
   }
+
+  List<Burc> veriKaynaginiHazirla() {
+
+    List<Burc> burclar = [];
+
+    for(int i = 0; i <12 ; i++) {
+
+      String  kucukResim = Strings.BURC_ADLARI[i].toLowerCase() + "${i + 1}.png"; // Koc -> koc1.png
+      String  buyukResim = Strings.BURC_ADLARI[i].toLowerCase() + "_buyuk"+ "${i + 1}.png"; // Koc -> koc _buyuk1.png
+
+      Burc eklenecekBurc = Burc (Strings.BURC_ADLARI[i], Strings.BURC_TARIHLERI[i], Strings.BURC_GENEL_OZELLIKLERI[i], kucukResim, buyukResim);
+      burclar.add(eklenecekBurc);
+    }
+
+    return burclar;
+
+  }
+
+
 
 }
