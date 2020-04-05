af830049527f7a035c3257c7ee801ceb57beb0e0
Mustafa <18701031@mersin.edu.tr>
2020-04-05 23:49:17 +0300

    Creating the Horoscope Detail Page

--- lib/burc_detay.dart
+++ lib/burc_detay.dart
@@ -1,8 +1,65 @@
+import 'package:com/burc_liste.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'models/burc.dart';
+
 class BurcDetay extends StatelessWidget {
+
+  int gelenIndex;
+  Burc secilenBurc;
+
+  BurcDetay(this.gelenIndex);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    secilenBurc = BurcListesi.tumBurclar[gelenIndex];
+
+    return Scaffold(
+
+      primary: true,
+
+      body: CustomScrollView(
+        primary: false,
+        slivers: <Widget>[
+
+          SliverAppBar(
+            expandedHeight: 250,
+            pinned: true,
+            backgroundColor: Colors.pink,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset("images/" + secilenBurc.burcBuyukResim, fit: BoxFit.cover,),
+              centerTitle: true,
+            ),
+            title: Text(secilenBurc.burcAdi + "Burcu ve Özellikleri"),
+          ),
+
+          SliverToBoxAdapter(
+
+            child: Container(
+              margin: EdgeInsets.all(8),
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.pink.shade400,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: SingleChildScrollView(
+
+                child: Text(secilenBurc.burcDetay, style: TextStyle(fontSize: 18, color: Colors.black)
+                ),
+            ),
+
+            ),
+
+          )
+
+        ],
+
+      ),
+
+    );
   }
+
+
 }
