6bb046aa9cc25bcbc4ca782965feb9f6baa63b56
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2020-08-03 22:53:45 +0300

    Pallet Generator Usage and Application is Over

--- lib/burc_liste.dart
+++ lib/burc_liste.dart
@@ -11,7 +11,7 @@ class BurcListesi extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Burç Rehberleri"),
+        title: Text("Burç Rehberi"),
       ),
       body: listeyiHazirla(),
     );
